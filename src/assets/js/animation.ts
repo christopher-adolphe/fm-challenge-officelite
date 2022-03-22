5dbd6278555fbcca468466fa365eecc7873c39b9
cadolphe <christopher.adolphe@gmail.com>
2022-03-22 07:04:49 +0400

    Feature (Animation): Add gsap scrollTrigger to animate pricing section

--- src/assets/js/animation.ts
+++ src/assets/js/animation.ts
@@ -1,19 +1,35 @@
 import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import CSSRulePlugin from 'gsap/CSSRulePlugin';
 import { getDOMElement } from './utilities';
 
 export default function animate() {
+  gsap.registerPlugin(ScrollTrigger, CSSRulePlugin);
   const homePageElem = getDOMElement('page-home');
   const signUpPageElem = getDOMElement('page-signup');
 
   if (homePageElem) {
-    const homeTimeline = gsap.timeline({
+    const heroTimeline = gsap.timeline({
       defaults: {
         ease: 'power4.inOut',
         duration: 2
       }
     });
 
-    homeTimeline.from('.hero__title', {
+    const pricingTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#section-pricing',
+        start: 'top 90%',
+        end: 'top 45%',
+        scrub: 4
+      },
+      defaults: {
+        ease: 'power4.inOut',
+        duration: 2
+      }
+    });
+
+    heroTimeline.from('.hero__title', {
       clipPath: 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)',
       y: '100%',
       opacity: 0,
@@ -35,6 +51,13 @@ export default function animate() {
       opacity: 0
     }, '-=2');
 
+    pricingTimeline.from('.card', {
+      stagger: 1.75,
+      scale: 0,
+      opacity: 0,
+      duration: 8
+    });
+
     return;
   }
 
@@ -45,13 +68,20 @@ export default function animate() {
         duration: 2
       }
     });
+    const bgPanel = CSSRulePlugin.getRule('.page--signup .page__content::after');
 
-    signupTimeline.from('.hero__title', {
+    signupTimeline.from(bgPanel, {
+      cssRule: {
+        clipPath: 'polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)',
+        opacity: 0
+      }
+    })
+    .from('.hero__title', {
       clipPath: 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)',
       y: '100%',
       opacity: 0,
       duration: 2.2
-    })
+    }, '-=2')
     .from('.hero__tag-line', {
       clipPath: 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)',
       y: '100%',
@@ -63,7 +93,7 @@ export default function animate() {
       opacity: 0
     }, '-=2')
     .from('.hero__form', {
-      clipPath: 'polygon(-40% 0, 0% 0, 0% 140%, -40% 140%)',
+      clipPath: 'polygon(-40% 0%, 0% 0%, 0% 140%, -40% 140%)',
       x: '-10%',
       opacity: 0
     }, '-=2');
